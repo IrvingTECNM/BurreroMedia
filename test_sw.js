668f948f76fa333d3ff91097bf20587e22720907
Irving Jesús Contreras Ramirez <l22170614@culiacan.tecnm.mx>
2026-04-23 00:27:00 -0700

    Funcional

--- test_sw.js
+++
@@ -1,6 +0,0 @@
-const { execSync } = require('child_process');
-const html = execSync('curl -s -L -H "User-Agent: Mozilla/5.0" "https://streamwish.to/e/ehg2ep6t0v8k"', { encoding: 'utf-8' });
-const match = html.match(/sources:\s*\[\s*\{\s*file:\s*["']([^"']+)["']/i);
-console.log(match ? match[1] : 'Not found');
-// Save for manual inspection just in case
-require('fs').writeFileSync('test_streamwish.html', html);
--- test_voe.js
+++
@@ -1,8 +0,0 @@
-﻿const fs = require('fs');
-const html = fs.readFileSync('scratch_voe2.html', 'utf8');
-const lines = html.split('\n');
-const scriptLine = lines.find(l => l.includes('DROH#') || l.includes('function(') || l.includes('hls'));
-if (scriptLine) {
-   fs.writeFileSync('voe_script.txt', scriptLine);
-   console.log('Script line extracted, length:', scriptLine.length);
-}
